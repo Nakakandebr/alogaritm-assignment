fe44b4b0a65a6a0a4e2b29fc68f48052ad3da127
Nakakandebr <bridgetnakakandea@gmail.com>
2023-03-22 17:12:48 +0300

    commit

--- algaritm.js
+++ algaritm.js
@@ -1,21 +1,21 @@
 //mergesort
-let num = [45,12,6,89,2,5]
+let n = [45,12,6,89,2,5]
   let target = 6;
 let firstIndex = 0;
-let lastIndex = num.length-1;
+let lastIndex = n.length-1;
 let middleIndex = Math.floor((firstIndex+lastIndex)/2);
-while(num[middleIndex]!==firstIndex<=lastIndex){
+while(n[middleIndex]!==firstIndex<=lastIndex){
     console.log(middleIndex);
     console.log(firstIndex);
     console.log(lastIndex);
-    if(num[middleIndex]>6){
+    if(n[middleIndex]>6){
         lastIndex=middleIndex-1;
     }  else{
         lastIndex=middleIndex+1;
     }
     middleIndex=Math.floor((firstIndex+lastIndex)/2);{
     }
-    return num[middleIndex]===lastIndex-1;
+    return n[middleIndex]===lastIndex-1;
     }
     //Given an unsorted array of numbers return the sorted array in descending order
     let arr=[123,89,5,23,9,56]
@@ -29,23 +29,23 @@ while(num[middleIndex]!==firstIndex<=lastIndex){
     }
     return[...arr ,...leftIndex,...rightIndex];
     }
-    function mergeSort(unsortedArray){
+    function merge(unsortedArray){
         const middleIndex = unsortedArray.length/2;
         if(unsortedArray.length<2){
             return unsortedArray;
         }
         const leftIndex = unsortedArray.splice(0,middleIndex);
-        return mergeArrays(mergeSort(leftIndex)),mergeSort(unsortedArray);
+        return mergeArrays(merge(leftIndex)),merge(unsortedArray);
     }
     //
-    function mergeSort(arr) {
+    function merge(arr) {
         if (arr.length <= 1) {
           return arr;
         }
         let mid = Math.floor(arr.length / 2);
         let left = arr.slice(0, mid);
         let right = arr.slice(mid);
-        return merge(mergeSort(left), mergeSort(right));
+        return merge(merge(left), merge(right));
       }
       function merge(left, right) {
         let result = [];
